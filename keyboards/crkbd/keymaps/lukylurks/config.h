7dd441f511b2ca5f4590914aab0f02354f6cd8a8
Luckner Cyné <luckner@cyne.fr>
2022-12-04 14:20:59 +0100

    fix missing ESC and accidental mods when typing

--- keyboards/crkbd/keymaps/lukylurks/config.h
+++ keyboards/crkbd/keymaps/lukylurks/config.h
@@ -29,7 +29,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 //#define TAPPING_FORCE_HOLD
-//#define TAPPING_TERM 100
+#define TAPPING_TERM 200
+#define IGNORE_MOD_TAP_INTERRUPT
 
 #define MK_3_SPEED
 #define MK_MOMENTARY_ACCEL
